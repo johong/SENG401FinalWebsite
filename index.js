8868fc7f1526aa983d79165135df126d76da1768
Joshua Hong <johong@ucalgary.ca>
2019-01-17 16:33:02 -0700

    add highlighting to contact and map

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-let name = document.getElementsByClassName("aboutUs-container");
+let aboutUs = document.getElementsByClassName('aboutUs-container');
+let contactInfo = document.getElementsByClassName('inputField');
+let map = document.getElementById('mapId');
 
-for (let i = 0; i < name.length; i++) {
-    name[i].addEventListener('mouseover', function() {
-        name[i].style.background = "yellow";
+for (let i = 0; i < aboutUs.length; i++) {
+    aboutUs[i].addEventListener('mouseover', function() {
+        aboutUs[i].style.background = 'yellow';
     });
 
-    name[i].addEventListener('mouseout', function() {
-        name[i].style.background = "white";
+    aboutUs[i].addEventListener('mouseout', function() {
+        aboutUs[i].style.background = 'white';
     });
 }
+
+for (let i = 0; i < contactInfo.length; i++) {
+    contactInfo[i].addEventListener('mouseover', function() {
+        contactInfo[i].style.background = 'yellow';
+    });
+
+    contactInfo[i].addEventListener('mouseout', function() {
+        contactInfo[i].style.background = 'white';
+    });
+}
+
+map.addEventListener('mouseover', function() {
+    map.style.background = 'yellow';
+});
+
+map.addEventListener('mouseout', function() {
+    map.style.background = 'white';
+});
