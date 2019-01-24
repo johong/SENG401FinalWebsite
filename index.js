96ef80ffe497a191e89b1ae0e56443ba51ef2220
Joshua Hong <johong@ucalgary.ca>
2019-01-24 11:56:12 -0700

    add extra section hover

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 let aboutUs = document.getElementsByClassName('aboutUs-container');
 let contactInfo = document.getElementsByClassName('inputField');
 let map = document.getElementById('mapId');
+let edmonton = {
+    Name: 'Edmonton',
+    Latitude: 53.5444,
+    Longitude: -113.4909,
+    Population: 960015,
+    Area: '684.37 SqKM'
+};
+
+edmonton['Density'] = (edmonton['Population']/684.37).toString() + ' SqKM';
+
 
 for (let i = 0; i < aboutUs.length; i++) {
     aboutUs[i].addEventListener('mouseover', function() {
+        document.getElementById('currentSectionName').innerHTML = 'About Us';
         highlightItem(aboutUs[i])
     });
 
     aboutUs[i].addEventListener('mouseout', function() {
+        document.getElementById('currentSectionName').innerHTML = '';
         unHighlightAboutUs(aboutUs[i])
     });
 }
 
 for (let i = 0; i < contactInfo.length; i++) {
     contactInfo[i].addEventListener('mouseover', function() {
+        document.getElementById('currentSectionName').innerHTML = 'Contact Us';
         highlightItem(contactInfo[i]);
     });
 
     contactInfo[i].addEventListener('mouseout', function() {
+        document.getElementById('currentSectionName').innerHTML = '';
         unHighlightItem(contactInfo[i]);
     });
 }
 
 map.addEventListener('mouseover', function() {
+    document.getElementById('currentSectionName').innerHTML = 'Map';
     highlightItem(map);
 });
 
 map.addEventListener('mouseout', function() {
+    document.getElementById('currentSectionName').innerHTML = '';
     unHighlightItem(map);
 });
 
